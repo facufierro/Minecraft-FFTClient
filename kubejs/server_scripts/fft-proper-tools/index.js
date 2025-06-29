9deb27f792fb9e8d9aac78a94ebd5f2ddd371c05
Facundo Fierro <fierrof_47@hotmail.com>
2025-06-29 21:54:46 +0200

    Refactor backpack and tool management: simplify event handling and remove unnecessary initialization

--- kubejs/server_scripts/fft-proper-tools/index.js
+++ kubejs/server_scripts/fft-proper-tools/index.js
@@ -1,16 +1,8 @@
-const ProperTools = {
-    initialize: () => {
-        BlockEvents.leftClicked(event => {
-            if (event.block.hasTag('minecraft:logs')) {
-                if (!event.player.mainHandItem.hasTag('minecraft:axes')) {
-                    event.player.tell(`§cYou need an axe to break logs!`)
-                    event.cancel()
-                }
-            }
-        })
-        
-        console.info('FFT Proper Tools: Events registered')
+BlockEvents.leftClicked(event => {
+    if (event.block.hasTag('minecraft:logs')) {
+        if (!event.player.mainHandItem.hasTag('minecraft:axes')) {
+            event.player.tell(`§cYou need an axe to break logs!`)
+            event.cancel()
+        }
     }
-}
-
-ProperTools
+})
